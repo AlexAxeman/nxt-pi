aa0b92d106342c0da96e9d778b503096b4006059
Alex Gout <alex.gout@shopify.com>
2020-01-13 20:39:04 -0500

    test motors on Rolo

--- rolocode/drive.py
+++ rolocode/drive.py
@@ -5,17 +5,20 @@ import nxt.bluesock
 from nxt.motor import *
 
 
-def spin_around(b):
-    m_left = Motor(b, PORT_B)
-    m_left.turn (127, 360)
-    m_right = Motor(b, PORT_C)
-    m_right.turn(127, 360)
+def run(motors, time):
+    for motor in motors:
+        motor.run(power=100)
 
+    sleep(time)
+
+    for motor in motors:
+        motor.idle()
 
 b = None
 try:
     b = nxt.bluesock.BlueSock('00:16:53:08:09:1F').connect()
-    spin_around(b)
+    motors = [Motor(b, PORT_B), Motor(b, PORT_C)]
+    run(motors, 10)
 except:
     print("Error while running test:")
     print(str(sys.exc_info()[1]))
